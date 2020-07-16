6f9ca0c688050882a8a14aad012e4422a250f85c
haurbano <haurbano.dev@gmail.com>
2020-07-16 02:53:05 -0500

    Using queries in post srvice

--- data/src/main/java/com/haurbano/data/retrofit/PostsService.kt
+++ data/src/main/java/com/haurbano/data/retrofit/PostsService.kt
@@ -5,8 +5,11 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface PostsService {
-    @GET("top/.json?t=all&limit=10")
-    suspend fun getPosts(): PostsRequestResponse
+    @GET("top/.json")
+    suspend fun getPosts(
+        @Query("t") t: String = "all",
+        @Query("limit") limit: String = "10"
+    ): PostsRequestResponse
 
     @GET("top/.json?")
     suspend fun getMorePosts(
